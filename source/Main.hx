0df90ace1ce9ff506eac977daa6ebbd9aca7b784
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-04 22:27:02 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -78,7 +78,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if cpp
+		#if windows
 	//	initialState = Caching;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#else
